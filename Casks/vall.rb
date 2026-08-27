9e80403933f6098f4207c9c6f6d5d167f7a1f8d1
nextster <tema@nextster.dev>
2026-08-28 03:48:27 +0400

    Update Vall to 1.0 beta 2

--- Casks/vall.rb
+++ Casks/vall.rb
@@ -1,6 +1,6 @@
 cask "vall" do
-  version "1.0-beta.1"
-  sha256 "3aa346d22434998633a66eec6e49ab0cc2dee90247f29e650b5fca380a0b7cf5"
+  version "1.0-beta.2"
+  sha256 "e478341696537c885149acbe90c8c677b4ddc053c134dfab56dd3d0cd362332f"
 
   url "https://github.com/nextster/vall-releases/releases/download/v#{version}/Vall-1.0.dmg"
   name "Vall"
