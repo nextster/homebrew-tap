982576ec77f148fabc4995fff5ad2d3033260824
nextster <tema@nextster.dev>
2026-08-31 11:55:40 +0400

    Update Tuck to 1.0-beta.2

--- Casks/tuck.rb
+++ Casks/tuck.rb
@@ -1,6 +1,6 @@
 cask "tuck" do
-  version "1.0-beta.1"
-  sha256 "4ac0f990a1c2c6d0e2a61892cad38c2bc87e29032027f1b87585b77e910e4315"
+  version "1.0-beta.2"
+  sha256 "ddd90ad819b4f4cef380c6f5aa9ac2078eefeb2d6bc03a27cee88534d7d64012"
 
   url "https://github.com/nextster/tuck-releases/releases/download/v#{version}/Tuck-1.0.dmg"
   name "Tuck"
