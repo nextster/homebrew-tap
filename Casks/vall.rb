5fcef86ea1228455484493d3d3fdb107a4f6c1ec
nextster <tema@nextster.dev>
2026-09-11 02:35:39 +0400

    Update Vall to 1.0 beta 6 with wallpaper visibility fix

--- Casks/vall.rb
+++ Casks/vall.rb
@@ -1,6 +1,6 @@
 cask "vall" do
-  version "1.0-beta.5"
-  sha256 "032b14dab2e3c33e8aa34abb7139cbd4f603e7c5c97bb20e2cd61b5c7a19ab30"
+  version "1.0-beta.6"
+  sha256 "be8e84bead94b616c0a83adbc56c171fd72be4d077c74c4470dc5f94cc6e0c3d"
 
   url "https://github.com/nextster/vall-releases/releases/download/v#{version}/Vall-1.0.dmg"
   name "Vall"
